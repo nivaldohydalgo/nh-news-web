39b9c5cfc22fa36db66a83bcfcab7bdea9dac9f1
Nivaldo Hydalgo <nivaldohydalgo@gmail.com>
2024-02-13 13:18:40 -0300

    1 commit 13/02/2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component, OnInit, isDevMode } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { TopHeaderComponent } from './pages/top-header/top-header.component'
+import { HeaderComponent } from './pages/header/header.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { FooterComponent } from './pages/footer/footer.component';
+
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet],
+    imports: [
+        RouterOutlet,
+        TopHeaderComponent,
+        HeaderComponent,
+        MenuComponent,
+        FooterComponent
+    ],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
