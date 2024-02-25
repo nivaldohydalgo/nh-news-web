df018739b05b80f8ef256244bf2c36962fc96990
Nivaldo Hydalgo <nivaldohydalgo@gmail.com>
2024-02-25 10:46:31 -0300

    commit 25/02/2024 manhã

--- src/app/pages/info/info.component.ts
+++ src/app/pages/info/info.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-info',
-  standalone: true,
-  imports: [],
-  templateUrl: './info.component.html',
-  styleUrl: './info.component.css'
+    selector: 'app-info',
+    standalone: true,
+    imports: [],
+    templateUrl: './info.component.html',
+    styleUrl: './info.component.css'
 })
 export class InfoComponent {
 
